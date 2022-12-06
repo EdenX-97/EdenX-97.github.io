6cf001de201331d1f6fa276f77fe1676c99f01e0
Mo Xu <edenx97@gmail.com>
2022-12-07 00:16:21 +1100

    create project

---
+++ src/.vuepress/config.ts
@@ -0,0 +1,18 @@
+import { defineUserConfig } from "vuepress";
+import theme from "./theme.js";
+
+export default defineUserConfig({
+  base: "/",
+
+  locales: {
+    "/": {
+      lang: "en-US",
+      title: "Docs Demo",
+      description: "A docs demo for vuepress-theme-hope",
+    },
+  },
+
+  theme,
+
+  shouldPrefetch: false,
+});
